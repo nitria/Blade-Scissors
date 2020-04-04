948868741587fc458e56e67b3c116c92259dfdb1
nick <ntriantafillidis@yahoo.gr>
2020-04-04 17:29:15 +0300

    enter

---
+++ contactform.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])) {
+    $first = $_POST['first'];
+    $last = $_POST['last'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "ntriantafillidis@yahoo.gr";
+    $headers = "From: ".$mailFrom;
+    $txt = "mail from ".$first." ".$last."\n\n".$message;
+
+    mail($mailTo, $message, $txt, $headers);
+    header("Location: index.html?send");
+}
